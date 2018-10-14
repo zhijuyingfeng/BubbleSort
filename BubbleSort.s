fab6cb035e8dbde9d3473c8e47f1b9971d1d1812
nigao <2604385867@qq.com>
2018-10-14 09:53:46 +0800

    update BubbleSort.s

--- BubbleSort.s
+++ BubbleSort.s
@@ -1,100 +1,148 @@
-	.file	"BubbleSort.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"%d\n"
-	.text
-	.globl	main
-	.type	main, @function
+$LC1:
+        .ascii  "%d\012\000"
+$LC0:
+        .word   8
+        .word   6
+        .word   9
+        .word   2
+        .word   3
+        .word   5
+        .word   4
+        .word   0
+        .word   1
+        .word   7
 main:
-.LFB5:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$64, %rsp
-	movq	%fs:40, %rax
-	movq	%rax, -8(%rbp)
-	xorl	%eax, %eax
-	movl	$8, -48(%rbp)
-	movl	$6, -44(%rbp)
-	movl	$9, -40(%rbp)
-	movl	$2, -36(%rbp)
-	movl	$3, -32(%rbp)
-	movl	$5, -28(%rbp)
-	movl	$4, -24(%rbp)
-	movl	$0, -20(%rbp)
-	movl	$1, -16(%rbp)
-	movl	$7, -12(%rbp)
-	movl	$0, -60(%rbp)
-	jmp	.L2
-.L6:
-	movl	$0, -56(%rbp)
-	jmp	.L3
-.L5:
-	movl	-56(%rbp), %eax
-	cltq
-	movl	-48(%rbp,%rax,4), %edx
-	movl	-56(%rbp), %eax
-	addl	$1, %eax
-	cltq
-	movl	-48(%rbp,%rax,4), %eax
-	cmpl	%eax, %edx
-	jbe	.L4
-	movl	-56(%rbp), %eax
-	cltq
-	movl	-48(%rbp,%rax,4), %eax
-	movl	%eax, -52(%rbp)
-	movl	-56(%rbp), %eax
-	addl	$1, %eax
-	cltq
-	movl	-48(%rbp,%rax,4), %edx
-	movl	-56(%rbp), %eax
-	cltq
-	movl	%edx, -48(%rbp,%rax,4)
-	movl	-56(%rbp), %eax
-	addl	$1, %eax
-	movl	-52(%rbp), %edx
-	cltq
-	movl	%edx, -48(%rbp,%rax,4)
-.L4:
-	addl	$1, -56(%rbp)
-.L3:
-	movl	$9, %eax
-	subl	-60(%rbp), %eax
-	cmpl	%eax, -56(%rbp)
-	jl	.L5
-	addl	$1, -60(%rbp)
-.L2:
-	cmpl	$8, -60(%rbp)
-	jle	.L6
-	movl	$0, -60(%rbp)
-	jmp	.L7
-.L8:
-	movl	-60(%rbp), %eax
-	cltq
-	movl	-48(%rbp,%rax,4), %eax
-	movl	%eax, %esi
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	addl	$1, -60(%rbp)
-.L7:
-	cmpl	$9, -60(%rbp)
-	jle	.L8
-	movl	$0, %eax
-	movq	-8(%rbp), %rcx
-	xorq	%fs:40, %rcx
-	je	.L10
-	call	__stack_chk_fail@PLT
-.L10:
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE5:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 7.3.0-16ubuntu3) 7.3.0"
-	.section	.note.GNU-stack,"",@progbits
+        addiu   $sp,$sp,-88
+        sw      $31,84($sp)
+        sw      $fp,80($sp)
+        move    $fp,$sp
+        lui     $2,%hi($LC0)
+        addiu   $3,$fp,36
+        addiu   $2,$2,%lo($LC0)
+        li      $4,40                 # 0x28
+        move    $6,$4
+        move    $5,$2
+        move    $4,$3
+        jal     memcpy
+        nop
+
+        sw      $0,24($fp)
+$L6:
+        lw      $2,24($fp)
+        nop
+        slt     $2,$2,9
+        beq     $2,$0,$L2
+        nop
+
+        sw      $0,28($fp)
+$L5:
+        li      $3,9                        # 0x9
+        lw      $2,24($fp)
+        nop
+        subu    $3,$3,$2
+        lw      $2,28($fp)
+        nop
+        slt     $2,$2,$3
+        beq     $2,$0,$L3
+        nop
+
+        lw      $2,28($fp)
+        nop
+        sll     $2,$2,2
+        addiu   $3,$fp,24
+        addu    $2,$3,$2
+        lw      $3,12($2)
+        lw      $2,28($fp)
+        nop
+        addiu   $2,$2,1
+        sll     $2,$2,2
+        addiu   $4,$fp,24
+        addu    $2,$4,$2
+        lw      $2,12($2)
+        nop
+        sltu    $2,$2,$3
+        beq     $2,$0,$L4
+        nop
+
+        lw      $2,28($fp)
+        nop
+        sll     $2,$2,2
+        addiu   $3,$fp,24
+        addu    $2,$3,$2
+        lw      $2,12($2)
+        nop
+        sw      $2,32($fp)
+        lw      $2,28($fp)
+        nop
+        addiu   $2,$2,1
+        sll     $2,$2,2
+        addiu   $3,$fp,24
+        addu    $2,$3,$2
+        lw      $3,12($2)
+        lw      $2,28($fp)
+        nop
+        sll     $2,$2,2
+        addiu   $4,$fp,24
+        addu    $2,$4,$2
+        sw      $3,12($2)
+        lw      $2,28($fp)
+        nop
+        addiu   $2,$2,1
+        lw      $3,32($fp)
+        sll     $2,$2,2
+        addiu   $4,$fp,24
+        addu    $2,$4,$2
+        sw      $3,12($2)
+$L4:
+        lw      $2,28($fp)
+        nop
+        addiu   $2,$2,1
+        sw      $2,28($fp)
+        b       $L5
+        nop
+
+$L3:
+        lw      $2,24($fp)
+        nop
+        addiu   $2,$2,1
+        sw      $2,24($fp)
+        b       $L6
+        nop
+
+$L2:
+        sw      $0,24($fp)
+$L8:
+        lw      $2,24($fp)
+        nop
+        slt     $2,$2,10
+        beq     $2,$0,$L7
+        nop
+
+        lw      $2,24($fp)
+        nop
+        sll     $2,$2,2
+        addiu   $3,$fp,24
+        addu    $2,$3,$2
+        lw      $2,12($2)
+        nop
+        move    $5,$2
+        lui     $2,%hi($LC1)
+        addiu   $4,$2,%lo($LC1)
+        jal     printf
+        nop
+
+        lw      $2,24($fp)
+        nop
+        addiu   $2,$2,1
+        sw      $2,24($fp)
+        b       $L8
+        nop
+
+$L7:
+        move    $2,$0
+        move    $sp,$fp
+        lw      $31,84($sp)
+        lw      $fp,80($sp)
+        addiu   $sp,$sp,88
+        j       $31
+        nop
