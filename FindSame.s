db92d4e8f3fb76f64bbb0df45712c13a4f6ffb54
nigao <2604385867@qq.com>
2018-10-15 19:46:27 +0800

    Finished

--- FindSame.s
+++ FindSame.s
@@ -1,7 +1,11 @@
 .data
     A: .word 83, 32, 37, 70, 98, 53, 38, 18, 98, 14, 32, 92, 14, 74, 61, 62, 99, 83, 39, 95
-    B: .word 98, 3, 78, 75, 31, 91, 62, 67, 99, 76, 79, 43, 31, 23, 47, 80, 32, 56, 77, 26, 37, 21, 77, 24, 37, 32, 88, 4, 18, 64
+    B: .word 98, 3, 78, 75, 31, 91, 62, 67, 99, 76, 79, 43, 31, 23, 47, 14, 32, 56, 77, 26, 37, 21, 77, 24, 37, 32, 88, 4, 18, 64
+    # A: .word 2, 4
+    # B: .word 6, 4
     S: .space 80#store the same elements
+    newline: .asciiz "\n"
+    NoSame: .asciiz "No Same!"
 .text
 
 .globl main
@@ -53,21 +57,92 @@ return:
     lw $t3, -12($sp)
     lw $t2, -8($sp)
     lw $t1, -4($sp)
-    jal $ra
+    jr $ra
 
 main:
 
     #Sort A array
     la $a0, A
     li $a1, 20
-    b BubbleSort
+    jal BubbleSort
     
     #Sort B array
     la $a0, B
     li $a1, 30
-    b BubbleSort
+    jal BubbleSort
+
+    li $t0, 0#i=0
+    li $t1, 0#j=0
+    li $t2, 0#k=0,which count the number of the same elements.
+    li $s0, 19
+    li $s1, 29
+
+    la $s2, A#A+i
+    la $s3, B#B+j
+    la $s7, S#S+k
+
+check:
+    bgt $t0, $s0, output#if(i>19) output
+    bgt $t1, $s1, output#if(j>29) output
+    lw $s4, 0($s2)#A[i]
+    lw $s5, 0($s3)#B[j]
+    bgt $s4, $s5, jIncre#if(A[i]>B[j]) j++
+    bgt $s5, $s4, iIncre#if(B[j]> A[i]) i++
+    beq $s4, $s5, equal#if(A[i]==B[j]) i++,j++
+
+jIncre:
+    addi $t1, $t1, 1#j++
+    addi $s3, $s3, 4#B backwards 4
+    b check
+iIncre:
+    addi $t0, $t0, 1#i++
+    addi $s2, $s2, 4#A backwards 4
+    b check
+
+equal:
+    sw $s4, 0($s7)
+    addi $s7, $s7, 4#S backwards 4
+    addi $t2, $t2, 1#k++
+    addi $t0, $t0, 1#i++
+    addi $t1, $t1, 1#j++
+    addi $s2, $s2, 4#A backwards 4
+    addi $s3, $s3, 4#B backwards 4
+    b check
+
+no_same:
+    li $v0, 4
+    la $a0, NoSame
+    syscall
+    b exit
+
+same:
+    addi $s7, $s7, 4
+    addi $t0, $t0, 1
+    b output_loop
+
+output:
+    beqz $t2, no_same
+
+    li $t0, 0#i=0
+    la $s7, S#$s7<-- S
+    addi $t2, $t2, -1#$t2<-- k-1
+    li $t1, -1#temp
+
+output_loop:
+    bgt $t0, $t2, exit#if(i>k-1) exit
+    lw $a0, 0($s7)
+    beq $a0, $t1, same#if(temp==S[i]) don't output
+    li $v0, 1
+    syscall
+    add $t1, $a0, $zero#temp=S[i]
+    la $a0, newline
+    li $v0, 4
+    syscall
+    addi $s7, $s7, 4
+    addi $t0, $t0, 1
+    b output_loop
 
 
-
+exit:
     li $v0, 10
     syscall
