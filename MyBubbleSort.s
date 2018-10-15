6e95af10f770ba6c47a204021829b908d49a03e0
nigao <2604385867@qq.com>
2018-10-15 15:55:37 +0800

    update array

--- MyBubbleSort.s
+++ MyBubbleSort.s
@@ -1,5 +1,5 @@
 .data 
-    arr: .space 40
+    arr: .word 5, 7, 7, 4, 1, 8, 8, 4, 2, 3
     i: .word -1
     ii: .word -1
     max: .word 8
@@ -11,26 +11,26 @@
 #8,6,9,2,3,5,4,0,1,7
 main:
     la $s6, arr
-    addi $t1, $zero, 8
-    sw $t1, 0($s6)
-    addi $t1, $zero, 6
-    sw $t1, 4($s6)
-    addi $t1, $zero, 9
-    sw $t1, 8($s6)
-    addi $t1, $zero, 2
-    sw $t1, 12($s6)
-    addi $t1, $zero, 3
-    sw $t1, 16($s6)
-    addi $t1, $zero, 5
-    sw $t1, 20($s6)
-    addi $t1, $zero, 4
-    sw $t1, 24($s6)
-    addi $t1, $zero, 0
-    sw $t1, 28($s6)
-    addi $t1, $zero, 1
-    sw $t1, 32($s6)
-    addi $t1, $zero, 7
-    sw $t1, 36($s6)
+    # addi $t1, $zero, 8
+    # sw $t1, 0($s6)
+    # addi $t1, $zero, 6
+    # sw $t1, 4($s6)
+    # addi $t1, $zero, 9
+    # sw $t1, 8($s6)
+    # addi $t1, $zero, 2
+    # sw $t1, 12($s6)
+    # addi $t1, $zero, 3
+    # sw $t1, 16($s6)
+    # addi $t1, $zero, 5
+    # sw $t1, 20($s6)
+    # addi $t1, $zero, 4
+    # sw $t1, 24($s6)
+    # addi $t1, $zero, 0
+    # sw $t1, 28($s6)
+    # addi $t1, $zero, 1
+    # sw $t1, 32($s6)
+    # addi $t1, $zero, 7
+    # sw $t1, 36($s6)
 
     lw $s1, i
     lw $s5, max
@@ -82,4 +82,4 @@ output:
 
 exit:
     li $v0,10
-    syscall
+    syscall 
